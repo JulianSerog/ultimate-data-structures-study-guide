0560103fae346a1dd9d60a46f9db37b9d0586a3e
Richard Saney <rnsaney@uga.edu>
2016-08-28 17:15:09 -0400

    added quicksort

--- src/com/company/SortingMethods.java
+++ src/com/company/SortingMethods.java
@@ -31,14 +31,46 @@ public class SortingMethods {
                 //swap
                 int temp = arr[i];
                 arr[i] = arr[min];
-                arr[min] = arr[i];
+                arr[min] = temp;
             }
         }//for
         return arr;
     }//selectionSort
 
     public static int[] quickSort(int[] arr) {
+        return quickSort(arr, 0, arr.length-1);
+    }
+
+    public static int[] quickSort(int[] arr, int lo, int hi) {
+        if(lo < hi)
+        {
+            int partitionPos = partition(arr, lo, hi);
+            quickSort(arr, lo, partitionPos -1);
+            quickSort(arr, partitionPos + 1, hi);
+        }
         return arr;
+    }
+
+    private static int partition(int[] arr, int lo, int hi) {
+        int pivot =  arr[hi]; //the pivot item could be any item in the array
+        int swapIndex = lo;
+        for(int index = swapIndex; index < hi; index++)
+        {
+            if(arr[index] <= pivot)
+            {
+                swap(arr, index, swapIndex);
+                swapIndex++;
+            }
+        }
+        swap(arr, swapIndex, hi);
+        return swapIndex;
+    }
+
+    private static void swap(int[] arr, int pos1, int pos2 )
+    {
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
     }
 
     public static int[] mergeSort(int[] arr) {
